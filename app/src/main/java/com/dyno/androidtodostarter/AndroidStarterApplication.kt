fc880ba5af2e2b01b9a6bff118a255496c833e87
dyno <dyno.jun@gmail.com>
2025-01-18 12:29:07 +0900

    add android-ci

---
+++ app/src/main/java/com/dyno/androidtodostarter/AndroidStarterApplication.kt
@@ -0,0 +1,8 @@
+/* License Header */
+package com.dyno.androidtodostarter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidStarterApplication : Application()
